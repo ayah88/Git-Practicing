42af86de3045437cc387429c165cd18c91e06839
Ayah Arif <aalkawni@gmail.com>
2020-07-04 11:53:59 -0400

    initial commit

---
+++ src/main/java/Presentation/GitTest/App.java
@@ -0,0 +1,18 @@
+package Presentation.GitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+
+
+    }
+}
---
+++ src/test/java/Presentation/GitTest/GitAdd.java
@@ -0,0 +1,10 @@
+package Presentation.GitTest;
+
+public class GitAdd {
+
+	public static void main(String[] args) {
+
+		System.out.println("I am Happy");
+	}
+
+}
